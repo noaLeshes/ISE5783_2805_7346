e5b2e6470a35ba99331bc85bb9d29fd173ea133c
Noa <noa.leshes@gmail.com>
2023-05-16 18:21:40 +0300

    more documentation

--- src/lighting/AmbientLight.java
+++ src/lighting/AmbientLight.java
@@ -13,9 +13,9 @@ public class AmbientLight
     public static AmbientLight NONE=new AmbientLight(Color.BLACK,Double3.ZERO);//A constant representing no ambient light, This ambient light is completely black and has no effect on the scene.
 
     /**
-     * Constructs an AmbientLight object with the given intensity and scaling factor.
-     * @param iA The intensity color of the ambient light.
-     * @param kA The scaling factor for the ambient light intensity.
+     * Constructs an AmbientLight object with the given intensity and scaling factor
+     * @param iA The intensity color of the ambient light
+     * @param kA The scaling factor for the ambient light intensity
      */
 	public AmbientLight(Color iA, Double3 kA)
 	{
@@ -23,9 +23,9 @@ public class AmbientLight
 	}
 	
 	 /**
-     * Constructs an AmbientLight object with the given intensity and scaling factor.
-     * @param iA The intensity color of the ambient light.
-     * @param kA The scaling factor for the ambient light intensity.
+     * Constructs an AmbientLight object with the given intensity and scaling factor
+     * @param iA The intensity color of the ambient light
+     * @param kA The scaling factor for the ambient light intensity
      */
 	public AmbientLight(Color iA, double kA)
 	{
@@ -33,7 +33,7 @@ public class AmbientLight
 	}
 	
 	 /**
-     * @return The intensity color of the ambient light.
+     * @return The intensity color of the ambient light
      */
 	public Color getIntensity() 
 	{
--- src/scene/Scene.java
+++ src/scene/Scene.java
@@ -6,32 +6,52 @@ import geometries.*;
 
 public class Scene 
 {
-	public String name;
-	public Color backgroundColor = Color.BLACK;
-	public AmbientLight ambientLight = AmbientLight.NONE;
-	public Geometries geometries = new Geometries();
 	
+	/**
+	 * @author Miri Ordentlich and Noa Leshes
+	 */
+	
+	public String name;// The name of the scene
+	public Color backgroundColor = Color.BLACK;// The background color of the scene
+	public AmbientLight ambientLight = AmbientLight.NONE;// The ambient light in the scene
+	public Geometries geometries = new Geometries();// The geometries that present in the scene
+	
+	/**
+     * Constructs a Scene object with the given name
+     * @param name The name of the scene
+     */
 	public Scene(String name) 
 	{
 		this.name = name;
 	}
 
-
-
+	/**
+     * Sets the background color of the scene
+     * @param backgroundColor The background color to set
+     * @return The current Scene object
+     */
 	public Scene setBackgroundColor(Color backgroundColor) 
 	{
 		this.backgroundColor = backgroundColor;
 		return this;
 	}
 
-
+	 /**
+     * Sets the ambient light in the scene
+     * @param ambientLight The ambient light to set
+     * @return The current Scene object
+     */
 	public Scene setAmbientLight(AmbientLight ambientLight) 
 	{
 		this.ambientLight = ambientLight;
 		return this;
 	}
 
-
+	/**
+     * Sets the geometries present in the scene
+     * @param geometries The geometries to set
+     * @return The current Scene object
+     */
 	public Scene setGeometries(Geometries geometries) 
 	{
 		this.geometries = geometries;
