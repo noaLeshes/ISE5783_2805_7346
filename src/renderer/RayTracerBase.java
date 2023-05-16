63a374062d3fd5744c6323ad9e0504468aac7395
MiriOrdentlich <mariaordent@gmail.com>
2023-05-16 19:04:41 +0300

    more of the Stage5

--- src/renderer/RayTracerBase.java
+++ src/renderer/RayTracerBase.java
@@ -1,5 +1,15 @@
 package renderer;
+import scene.*;
+import primitives.*;
 
-public class RayTracerBase {
-
+public abstract class  RayTracerBase 
+{
+	protected Scene scene;
+	
+	public RayTracerBase(Scene scene1) 
+	{
+		this.scene = scene1;
+	}
+	
+	public abstract Color traceRay(Ray ray) throws IllegalArgumentException;
 }
--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -1,5 +1,20 @@
 package renderer;
 
-public class RayTracerBasic {
+import scene.Scene;
+import primitives.*;
 
+public class RayTracerBasic extends RayTracerBase
+{
+	public RayTracerBasic(Scene scene)
+	{
+		super(scene);
+	}
+	
+	@Override
+	public primitives.Color traceRay(Ray ray) throws IllegalArgumentException 
+	{
+		Point closestPoint = ray.findClosestPoint(scene.geometries.findIntsersections(ray));
+		return closestPoint == null ? scene.backgroundColor : scene.ambientLight.getIntensity();
+		
+	}
 }
